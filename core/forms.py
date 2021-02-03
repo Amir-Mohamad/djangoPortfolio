d94516e6cc7997901ebdc2d7e8345fdb21c76097
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-03 13:05:35 +0330

    fix bugs

--- core/forms.py
+++ core/forms.py
@@ -7,7 +7,7 @@ class ContactUsForm(forms.ModelForm):
         fields = ('name', 'email', 'phone', 'message')
         widgets = {
             'name': forms.TextInput(attrs={"class": "form-control", "id":"name","placeholder": "Name","data-validation-required-message":"Please enter your name."}),
-            'email': forms.TextInput(attrs={"class": "form-control", "id":"phone", "placeholder": "Email", "data-validation-required-message":"Please enter your phone"}),
+            'phone': forms.TextInput(attrs={"class": "form-control", "id":"phone", "placeholder": "Phone", "data-validation-required-message":"Please enter your phone"}),
             'email': forms.TextInput(attrs={"class": "form-control", "id":"email", "placeholder": "Email", "data-validation-required-message":"Please enter your email"}),
             'message': forms.TextInput(attrs={"class": "form-control", "id":"message","placeholder": "Message.", "data-validation-required-message":"Please enter your Message"}),
         }
--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ContactUsModel(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=100)
     message = models.CharField(max_length=300)
 
     def __str__(self):
